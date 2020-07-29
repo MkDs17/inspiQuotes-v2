e165cb5ecc140e1c155ce5b23c398d34860a8a2d
MkDs17 <mickael.rassicot@gmail.com>
2020-07-29 16:45:29 +0200

    add Header Component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,11 +4,13 @@ import './app.scss';
 
 import Background from '../Background';
 import Container from '../Container';
+import Header from '../Header';
 
 const App = () => {
   return (
     <div id="app">
       <Background />
+      <Header />
       <Container />
     </div>
   );
