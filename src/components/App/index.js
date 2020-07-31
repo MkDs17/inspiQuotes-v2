a041e2db095b647694ded408267c0c3d7bd68c99
MkDs17 <mickael.rassicot@gmail.com>
2020-07-31 09:10:10 +0200

    test compress images

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,15 +7,13 @@ import Container from '../Container';
 import Footer from '../Footer';
 import Header from '../Header';
 
-const App = () => {
-  return (
-    <div id="app">
-      <Background />
-      <Header />
-      <Container />
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div id="app">
+    <Background />
+    <Header />
+    <Container />
+    <Footer />
+  </div>
+);
 
 export default App;
