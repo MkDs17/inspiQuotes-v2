2b0de754544c8b1351eba65c4c73db687b927812
MkDs17 <mickael.rassicot@gmail.com>
2020-08-01 10:42:14 +0200

    update quotes data

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -7,8 +7,8 @@ export default [
   },
   {
     id: 2,
-    text: 'Tu vis pour apprendre, et non être parfait',
-    author: 'Unknown',
+    text: 'Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure. It is our light, not our darkness that most frightens us.',
+    author: 'Marianne Williamson',
     likes: 24,
   },
   {
@@ -19,20 +19,26 @@ export default [
   },
   {
     id: 4,
-    text: 'Il n\'y a pas de vent favorable pour celui qui ne sait pas où il va',
-    author: 'SÉNÈQUE',
-    likes: 24,
-  },
-  {
-    id: 5,
-    text: 'Il y a ce que la vie t\'as donné, et ce que tu en fait',
-    author: 'Unknown',
+    text: 'Nothing binds you except your thoughts; nothing limits you except your fear; and nothing controls you except your beliefs.',
+    author: 'Marianne Williamson',
     likes: 42,
   },
   {
     id: 5,
-    text: 'Ce que tu fuis te poursuit, ce à quoi tu fais face, s\'efface',
-    author: 'Unknown',
+    text: 'If I have seen further, it is by standing on the shoulders of giants.',
+    author: 'Isaac Newton',
+    likes: 24,
+  },
+  {
+    id: 6,
+    text: 'When life gives you lemons, make lemonade',
+    author: 'Elbert Hubbart',
+    likes: 42,
+  },
+  {
+    id: 7,
+    text: 'There are those that look at things the way they are, and ask why? I dream of things that never were, and ask why not?',
+    author: 'George Bernard Shaw',
     likes: 24,
   },
 ];
